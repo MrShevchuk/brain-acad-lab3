b53b371e6c260e2adab31755a17b8c9a62ea74e1
MrShevchuk <viacheslavshevchuk911@gmail.com>
2020-03-22 16:46:54 +0200

    proj 3 to Github

---
+++ src/company/Main.java
@@ -0,0 +1,51 @@
+package company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // 1 задание
+
+	byte b = 100;
+        System.out.println(b);
+
+        short s = 1000;
+        System.out.println(s);
+
+        int i = 10000;
+        System.out.println(i);
+
+        long l = 100000;
+        System.out.println(l);
+
+        float f = 1000000;
+        System.out.println(f);
+
+        double d = 100000000;
+        System.out.println(d);
+
+        int c = (char) 1000001;
+        System.out.println(c);
+
+        boolean b1= true;
+        System.out.println(b1);
+
+// 2 задание
+
+        int j = 2147483647;
+        float g;
+        g = j;
+        System.out.println(g);
+        System.out.println(j);
+
+        // 3 задание
+
+        byte w = 127;
+         w++;
+        System.out.println(w);
+
+        // 4 задание
+
+
+    }
+}
